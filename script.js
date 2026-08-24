67757b6e31a958db61c1a946efd8fe4615ca1b9d
yakshibatish <yakshi0726.becse24@chitkara.edu.in>
2026-08-24 20:57:54 +0530

    add task functionaity

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("My to do list is working!");
+// console.log("My to do list is working!");
+function addtask(){
+let taskinput=document.getElementById("taskinput");
+let task=taskinput.value;
+let li=document.createElement("li");
+li.textContent=task;
+document.getElementById("tasklist").appendChild(li);
+taskinput.value="";
+}
