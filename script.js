527bb29c7dbcf76502d10c807bc777af9ba20766
yakshibatish <yakshi0726.becse24@chitkara.edu.in>
2026-08-26 12:54:37 +0530

    made some changes

--- script.js
+++ script.js
@@ -2,13 +2,22 @@ function addTask() {
     let input = document.getElementById("taskInput");
     let task = input.value;
 
-    // if (task === "") {
-    //     alert("Please enter a task!");
-    //     return;
-    // }
+    if (task === "") {
+        alert("Please enter a task!");
+        return;
+    }
 
     let li = document.createElement("li");
-    li.innerText = task;
+
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+
+    checkbox.onclick = function() {
+        li.style.textDecoration = "line-through";
+    };
+
+    li.appendChild(checkbox);
+    li.appendChild(document.createTextNode(task));
 
     document.getElementById("taskList").appendChild(li);
 
